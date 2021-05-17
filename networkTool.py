055f5b211c8e862dcebedc7dcf2fe098d7f951b5
Aissa-cmd <70541804+Aissa-cmd@users.noreply.github.com>
2021-05-17 22:34:10 +0100

    Adding the output fanctionality
    giving the ability to save the output in a csv file with a custom name

--- networkTool.py
+++ networkTool.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 from ipaddress import IPv4Network
+from functools import wraps
 from IPy import IP
 from termcolor import cprint
 import argparse
 import math
+import csv
+from datetime import datetime
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--network', dest="cidr", required=True, help="The CIDR notation of a network")
@@ -14,14 +18,16 @@ parser.add_argument('--subnetworks', nargs="*", type=int, help="A list of number
 parser.add_argument('--subnet-technique', choices=['VLSM', 'FLSM'], help="Choose the technique to use when subnetting the network either VLSM or FLSM (required when using the --subnetworks option)")
 parser.add_argument('--subnet-info', action="store_true", help="Show Info section for each subnetwork")
 parser.add_argument('--available', action="store_true", help="Show the available Networks after subnetting")
-#parser.add_argument('-o', '--output', action="store_true", help="Save output to a csv file")
-#parser.add_argument('-on', '--output-name', help="Save csv file with custom file name (default: subnitting_CIDR.csv)")
+parser.add_argument('-o', '--output', action="store_true", help="Save output to a csv file")
+parser.add_argument('-on', '--output-name', help="Save csv file with custom file name (default: YYMMDD_HHMMSS_subnet_technique_network.csv)")
+
 
 args = parser.parse_args()
 
 
 def section_legend(legend):
     def _outer_wrapper(func):
+        @wraps(func)
         def _inner_wrapper(*args, **kwargs):
             print(f"\n===================[{legend}]=====================")
             func(*args, **kwargs)
@@ -40,6 +46,10 @@ def info(cidr,  prefix_text="", color="white"):
     cprint(f"{prefix_text}Range of IPs    : {cidr[1]}-{cidr[-2]}", color, attrs=["bold"])
 
 
+def getStringInfo(numhosts ,cidr):
+    return f"{numhosts},{str(cidr)},{cidr.netmask},{IP(str(cidr), make_net=True).iptype()},{str(cidr).split('/')[0]},{cidr.broadcast_address},{cidr.num_addresses-2},{cidr[1]},{cidr[-2]}"
+
+
 @section_legend("Belongs")
 def contains(network, ips):
     network = IP(str(network), make_net=True)
@@ -54,6 +64,20 @@ def contains(network, ips):
 
 q = []
 sub_networks = []
+subnetworks_dict = []
+"""
+{
+    'numhosts': ,
+    'cidr': ,
+    'netmask': ,
+    'iptype': ,
+    'netid': ,
+    'broadcastip': ,
+    'usableip': ,
+    'firstip': ,
+    'lastip': 
+}
+"""
 
 
 @section_legend("VLSM Subnetting")
@@ -71,7 +95,7 @@ def subnet_vlsm(network, subnetworks):
             continue
         except IndexError:
             cprint(f"\n[!!] The network {str(network)} can't cover all the subetworks\n     these networks are not covered {str(subnetworks)}\n", "red", attrs=["bold"])
-            break
+            raise Exception("Can't cover all networks")
         subnetworks.pop()
         q.insert(0, new_snet.subnets(new_prefix=snet_prefix_len))
         new_snet = next(q[0])
@@ -81,6 +105,8 @@ def subnet_vlsm(network, subnetworks):
         cprint(f"({snet-2} Hosts)", "green", attrs=["bold"])
         if args.subnet_info:
             info(new_snet, prefix_text="\t| ")
+        if args.output:
+            subnetworks_dict.append(getStringInfo(snet-2, new_snet))
         i += 1    
 
 
@@ -97,7 +123,7 @@ def subnet_flsm(network, subnetworks):
         except:
             q.pop()
             cprint(f"\n[!!] The network {str(network)} can't cover all the subetworks\n     these networks are not covered {str(subnetworks)}\n", "red", attrs=["bold"])
-            break
+            raise Exception("Can't cover all networks")
         snet = subnetworks.pop()
         sub_networks.append(str(sn))
         print(f"{i:2d}", end=") ")
@@ -105,6 +131,8 @@ def subnet_flsm(network, subnetworks):
         cprint(f"({snet} Hosts)", "green", attrs=["bold"])
         if args.subnet_info:
             info(sn, prefix_text="\t| ")
+        if args.output:
+            subnetworks_dict.append(getStringInfo(snet-2, new_snet))
         i += 1
 
 
@@ -125,6 +153,20 @@ def available(network):
             i += 1
 
 
+def getfile_name():
+    return f"{datetime.now().strftime('%Y%m%d_%H%M%S')}_{args.subnet_technique}_{args.cidr.split('/')[0]}.csv"
+
+
+def save_output(file_name=None):
+    if file_name is None:
+        file_name = getfile_name()
+    with open(file_name, "w") as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(['Number of hosts', 'Network', 'Network mask', 'IP type', 'Network ID', 'Broadcast IP', 'Usable IPs', 'First IP', 'Last IP'])
+        for snet in subnetworks_dict:
+            csv.writer.writerow(snet.split(','))
+    
+
 def main():
     cidr = args.cidr
     try:
@@ -141,11 +183,19 @@ def main():
         subnets = args.subnetworks
         subnets.sort()
         if args.subnet_technique.upper() == "FLSM":
-            subnet_flsm(net, subnets)
+            try:
+                subnet_flsm(net, subnets)
+            except:
+                return
         else:
-            subnet_vlsm(net, subnets)
-    if args.available:
-        available(cidr)
+            try:
+                subnet_vlsm(net, subnets)
+            except:
+                return
+        if args.available:
+            available(cidr)
+        if args.output:
+            save_output(args.output_name)
 
 
 if __name__ == "__main__":
